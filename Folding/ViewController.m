e4b6253eece980b6a48b043fb6256fbbc934527c
Alexey Karasev <karasev.alexey@gmail.com>
2016-03-16 20:10:29 +0300

    initial commit

---
+++ Folding/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Folding
+//
+//  Created by Алексей Карасев on 16/03/16.
+//  Copyright © 2016 Алексей Карасев. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
